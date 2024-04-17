ae0c2e07c794ac54b1360d7d8787441484e60c75
ESzPa <peterszukk@gmail.com>
2024-04-17 11:02:35 +0200

    little modifications

--- feladat.cpp
+++ feladat.cpp
@@ -1,6 +1,5 @@
 #include "feladat.hpp"
 #include "moves.hpp"
-#include <unistd.h>
 
 void Feladat(){
 
--- window.cpp
+++ window.cpp
@@ -16,7 +16,7 @@ Rectangle startbtn = {1250, 25, 325, 125};
 
 int main(){
     InitWindow(window.width, window.height, window.name.c_str());
-    SetTargetFPS(60);
+    SetTargetFPS(120);
     KareszInit();
 
     while(!CheckCollisionPointRec(GetMousePosition(), startbtn) && !IsMouseButtonPressed(KEY_LEFT)){
